38c1e5c4792e5810877c31331148b13ef0a3e5e8
Balaje Jeyakodi <balajej@gmail.com>
2020-05-04 23:46:31 -0400

    generates unique, same and different records separately in three files

--- JsonComparison/Compare.py
+++ JsonComparison/Compare.py
@@ -1,7 +1,13 @@
 from Parser import *
-from operator import itemgetter
 
-differ = Parser(open('fileA.dat').readlines(), open('fileB.dat').readlines())
+with open('fileA.dat') as f1:
+    mylist1 = f1.read().splitlines()
+
+with open('fileB.dat') as f2:
+    mylist2 = f2.read().splitlines()
+
+#differ = Parser(open('fileA.dat').readlines(), open('fileB.dat').readlines())
+differ = Parser(mylist1, mylist2)
 
 same_record_file = open('samerecords.txt', 'w')
 unique_record_file = open('uniquerecords.txt', 'w')
@@ -10,20 +16,24 @@ for x in differ:
     print(x)
     if 0 in x.values():
         print('Same records are found')
-        for value in x.values():
-            same_record_file.write('{}'.format(value).strip())
+        for value in x:
+            if value == "Record":
+                same_record_file.write('{}\n'.format(x.get('Record')))
 
     elif 1 in x.values():
         print('Unique Records are found')
-        for value in x.values():
-            unique_record_file.write('{}'.format(value).strip())
+        for value in x:
+            if value == "Record":
+                unique_record_file.write('{}\n'.format(x.get('Record')))
 
     elif 2 in x.values():
         print('Unique Records are found')
-        for value in x.values():
-            unique_record_file.write('{}'.format(value).strip())
+        for value in x:
+            if value == "Record":
+                unique_record_file.write('{}\n'.format(x.get('Record')))
 
     elif 3 in x.values():
         print('Different Records are found')
-        for value in x.values():
-            different_record_file.write('{}'.format(value).strip())
+        for value in x:
+            if value == "Record":
+                different_record_file.write('{}\n'.format(x.get('Record')))
